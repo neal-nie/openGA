972982bf2a7ca2955bfa7476e50e24e5fb16e424
Guangze Nie <guangze.nie@outlook.com>
2021-09-08 08:32:22 +0800

    delete unnecessary comment

--- setup.py
+++ setup.py
@@ -70,51 +70,9 @@ setup(
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     packages=find_packages(include=("openGA*",)),
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=install_requires,
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    # extras_require={
-    #     "decode": ["cChardet==2.1.5", "chardet"],
-    #     "export": [
-    #         "fastparquet",
-    #         "h5py",
-    #         "hdf5storage>=0.1.17",
-    #         "scipy",
-    #         "python-snappy",
-    #     ],
-    #     "gui": [
-    #         "lxml",
-    #         "natsort",
-    #         "psutil",
-    #         "PyQt5>=5.14.0",
-    #         "pyqtgraph>=0.12.1",
-    #         "pyqtlet",
-    #         "PyQtWebEngine",
-    #     ],
-    #     "encryption": "cryptography",
-    #     "symbolic_math": "numexpr3",
-    # },
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    # package_data={"openGA": ["openGA/config/*.csv"]},
-    # include_package_data=True,
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #    data_files=[('my_data', ['data/data_file'])],
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    # entry_points={"console_scripts": ["openGA = openGA.gui.openGAgui:main [gui,export,decode]"]},
+    install_requires=install_requires
 )
